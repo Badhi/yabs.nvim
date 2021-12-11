1d51aa176230707f0a1eb5a9dba2f215e1087500
badhi <bmadhikarinayake@gmail.com>
2021-12-11 12:49:09 +0530

    adding plenary job for echo

--- lua/yabs/outputs/echo.lua
+++ lua/yabs/outputs/echo.lua
@@ -1,6 +1,23 @@
-local function echo(cmd)
-  print(vim.fn.system(cmd))
+local utils = require('yabs.utils')
+local Job = require('plenary.job')
+
+local function echo(_, data)
+  print(vim.fn.system(data))
+end
+
+local function echo_async(cmd, opts)
+  opts = opts or {}
+
+  local splitted_cmd = utils.split_cmd(cmd)
+  local job = Job:new({
+    command = table.remove(splitted_cmd, 1),
+    args = splitted_cmd,
+    on_stdout = vim.schedule_wrap(echo),
+    on_stderr = vim.schedule_wrap(echo),
+    on_exit = vim.schedule_wrap(opts.on_exit),
+  })
+  job:start()
 end
 
 local Output = require('yabs.output')
-return Output:new(echo)
+return Output:new(echo_async)
